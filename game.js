6135eaa09f1a9855e89fae97074c874adfe5c435
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2021-01-08 15:12:15 +0530

    TheBeatles

--- game.js
+++ game.js
@@ -1,15 +1,15 @@
 function confirmGame() {
-  const name = prompt("You Sure?(yes or no)");
-  const lname = name.toLowerCase();
-  if(lname == 'yes'){
-    console.log(name);
+  document.getElementById("game_start").textContent = null;
+  const conf = prompt("You Sure?(yes or no)");
+  const conf2 = conf.toLowerCase();
+  if (conf2 == "yes") {
+    const name = prompt("Whats your name?");
     const h1 = document.createElement("h1");
     const text = document.createTextNode("Hello " + name);
     h1.setAttribute("id", "name_of_player");
     h1.appendChild(text);
     document.getElementById("game_start").appendChild(h1);
-  }
-  else{
+  } else {
     window.location.href = "index.html";
   }
 }
