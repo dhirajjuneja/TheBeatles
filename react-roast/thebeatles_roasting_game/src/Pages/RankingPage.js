0622d7dcdf90344b2d4cdeac96424a0aaf08fea2
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2021-01-09 01:38:45 +0530

    TheBeatles

---
+++ react-roast/thebeatles_roasting_game/src/Pages/RankingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/dist/rank.css';
+
+function RankingPage() {
+    return (
+        <div>
+            RankingPage
+        </div>
+    )
+}
+
+export default RankingPage
