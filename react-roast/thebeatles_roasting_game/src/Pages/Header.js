00bfa3a0f18ca44521daf151e67bd7fba4c2ec6f
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2021-01-09 01:38:08 +0530

    TheBeatles

---
+++ react-roast/thebeatles_roasting_game/src/Pages/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <div>
+        <header class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="container-fluid justify-content-center align-items-center">
+            <a class="navbar-brand" href="/">
+              TheBeatles
+            </a>
+          </div>
+        </header>
+      </div>
+    );
+}
+
+export default Header
