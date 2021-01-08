44cab5ce75e0d5922e9e988104e9af3d170cf93b
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2021-01-08 14:54:51 +0530

    TheBeatles

---
+++ dist/game.dev.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function startGame() {
+  var name = prompt("Your Name?");
+  console.log(name);
+  var h1 = document.createElement('h1');
+  var text = document.createTextNode('Hello' + name);
+  h1.setattri;
+}
